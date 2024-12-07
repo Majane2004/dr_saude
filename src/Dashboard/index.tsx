721d0102af9d0f3af1bbb17920d01b913c7ebfc5
Karen Penedo <penedok12@gmail.com>
2024-12-07 13:44:39 -0400

    fix: tela dr saude

---
+++ src/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { PrincipalDiv } from "./style"
+
+
+export default function Dashboard(){
+    return(
+        <PrincipalDiv>
+            <h2>Dr Saúde</h2>
+        </PrincipalDiv>
+    )
+}
---
+++ src/Dashboard/style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+
+
+export const PrincipalDiv = styled.div `
+
+ background:'red'
+
+`
+
+
+
+
+
